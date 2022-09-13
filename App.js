aeffd60dae8efcdb65e9b12582109b13cdacfdaf
techdevcodeweb <tinyiko13@yahoo.com>
2022-09-13 14:15:43 +0200

    second commit

--- App.js
+++ App.js
@@ -5,25 +5,23 @@ import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default function App() {
 
-  const [titleText, setTitleText] = useState("Wishing you God's Blessings On Your Birthday");
+  const [titleText, setTitleText] = useState("Wishing you God's Blessings On Your Birthday!!!");
   const [bodyText, setBodyText] = useState("As you celebrate your birthday today, May God's blessings be with you always. May He surround you with tender loving care and may His presence be ever near. May He make all your dreams come true not just today, but all year through. May the good Lord continue to shower you with happiness, good fortune, prosperity and good health all the days of your life.");
   const [lastText, setLastText] = useState("Happy -->BIRTHDAY<-- to you");
 
   return (
     <View style={styles.container}>
-      <Text style={styles.titleText}>{titleText}
-        {"\n"}</Text>
       <Image
         style={styles.stretch}
-        source={require('./assets/birthday.png')}
+        source={require('./assets/butterflyflower.png')}
       />
-      <Text style={styles.bodyText}>{bodyText}
-      {"\n"}</Text>
-      <Text style={styles.lastText}>{lastText}
-      {"\n"}</Text>
+       <Text style={styles.titleText}>{titleText}
+        {"\n"}</Text>
+      <Text style={styles.bodyText}>{bodyText}</Text>
+      <Text style={styles.lastText}>{lastText}</Text>
           <Image
         style={styles.tinyPic}
-        source={require('./assets/butterflyflower.png')}
+        source={require('./assets/flower.png')}
       />
       
       <StatusBar style="auto" />
@@ -40,21 +38,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   titleText: {
-    fontSize: 20,
+    width: 350,
+    fontSize: 15,
     fontWeight: "bold",
-    color: 'purple'
+    color: 'green'
   },
   stretch: {
-    width: 700,
-    height: 270,
+    width: 400,
+    height: 200,
     resizeMode: 'stretch',
   },
   bodyText: {
-    width: 600,
-    fontFamily: 'Helvetica',
-    fontSize: 20,
-    lineHeight: 32,
-    fontWeight: "500",
+    width: 300,
+    fontFamily: 'serif',
+    fontSize: 15,
+    lineHeight: 18,
+    fontWeight: "900",
     fontStyle: 'italic',
     color: 'purple'
   },
@@ -64,7 +63,7 @@ const styles = StyleSheet.create({
     color: 'blue'
   },
   tinyPic: {
-    width: 700,
-    height: 300,
+    width: 400,
+    height: 200,
   },
 });
