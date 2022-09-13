eb6d7682e2576f89a63660e0a909a51e1c513b65
techdevcodeweb <tinyiko13@yahoo.com>
2022-09-13 13:07:28 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
+import React, { useState } from "react";
+
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View } from 'react-native';
 
 export default function App() {
+
+  const [titleText, setTitleText] = useState("Wishing you God's Blessings On Your Birthday");
+  const [bodyText, setBodyText] = useState("As you celebrate your birthday today, May God's blessings be with you always. May He surround you with tender loving care and may His presence be ever near. May He make all your dreams come true not just today, but all year through. May the good Lord continue to shower you with happiness, good fortune, prosperity and good health all the days of your life.");
+  const [lastText, setLastText] = useState("Happy -->BIRTHDAY<-- to you");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.titleText}>{titleText}
+        {"\n"}</Text>
+      <Image
+        style={styles.stretch}
+        source={require('./assets/birthday.png')}
+      />
+      <Text style={styles.bodyText}>{bodyText}
+      {"\n"}</Text>
+      <Text style={styles.lastText}>{lastText}
+      {"\n"}</Text>
+          <Image
+        style={styles.tinyPic}
+        source={require('./assets/butterflyflower.png')}
+      />
+      
       <StatusBar style="auto" />
     </View>
+    
   );
 }
 
@@ -16,5 +38,33 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: 'purple'
+  },
+  stretch: {
+    width: 700,
+    height: 270,
+    resizeMode: 'stretch',
+  },
+  bodyText: {
+    width: 600,
+    fontFamily: 'Helvetica',
+    fontSize: 20,
+    lineHeight: 32,
+    fontWeight: "500",
+    fontStyle: 'italic',
+    color: 'purple'
+  },
+  lastText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: 'blue'
+  },
+  tinyPic: {
+    width: 700,
+    height: 300,
   },
 });
